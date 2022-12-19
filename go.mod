650e776e4c0caa6e645f74115e46ebd8536b5d28
Tamal Saha <tamal@appscode.com>
2022-12-19 15:31:51 +0600

    first implementation
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ go.mod
@@ -0,0 +1,92 @@
+module kmodules.xyz/go-containerregistry
+
+go 1.18
+
+require (
+	github.com/awslabs/amazon-ecr-credential-helper/ecr-login v0.0.0-20221216172807-aacaa2a41a47
+	github.com/chrismellard/docker-credential-acr-env v0.0.0-20221129204813-6a4d6ed5d396
+	github.com/google/go-containerregistry v0.12.1
+	github.com/google/go-containerregistry/pkg/authn/k8schain v0.0.0-20221213180026-23d895d08035
+	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10
+	k8s.io/client-go v0.25.3
+)
+
+require (
+	cloud.google.com/go/compute v1.10.0 // indirect
+	github.com/Azure/azure-sdk-for-go v67.0.0+incompatible // indirect
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.28 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.21 // indirect
+	github.com/Azure/go-autorest/autorest/azure/auth v0.5.11 // indirect
+	github.com/Azure/go-autorest/autorest/azure/cli v0.4.6 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.17.3 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.5 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.5 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ecr v1.17.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ecrpublic v1.13.22 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.27 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.17.7 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/containerd/stargz-snapshotter/estargz v0.12.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dimchansky/utfbom v1.1.1 // indirect
+	github.com/docker/cli v20.10.20+incompatible // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.20+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gnostic v0.6.9 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-containerregistry/pkg/authn/kubernetes v0.0.0-20221017135236-9b4fdd506cdd // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/vbatts/tar-split v0.11.2 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/oauth2 v0.1.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.26.0 // indirect
+	k8s.io/apimachinery v0.26.0 // indirect
+	k8s.io/klog/v2 v2.80.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
+	k8s.io/utils v0.0.0-20221107191617-1a15be271d1d // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
---
+++ name/lib_test.go
@@ -0,0 +1,215 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package name
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-containerregistry/pkg/name"
+)
+
+func TestParseReference(t *testing.T) {
+	type args struct {
+		s    string
+		opts []name.Option
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Image
+		wantErr bool
+	}{
+		{
+			name: "Test with valid image string",
+			args: args{
+				s: "docker.io/library/nginx@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+			},
+			want: &Image{
+				Original:   "docker.io/library/nginx@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+				Name:       "index.docker.io/library/nginx@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+				Registry:   "index.docker.io",
+				Repository: "library/nginx",
+				Tag:        "",
+				Digest:     "sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with valid image string",
+			args: args{
+				s: "docker.io/library/mariadb@sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+			},
+			want: &Image{
+				Original:   "docker.io/library/mariadb@sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+				Name:       "index.docker.io/library/mariadb@sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+				Registry:   "index.docker.io",
+				Repository: "library/mariadb",
+				Tag:        "",
+				Digest:     "sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with valid image string with tag",
+			args: args{
+				s: "docker.io/library/mariadb:10.8.2@sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+			},
+			want: &Image{
+				Original:   "docker.io/library/mariadb:10.8.2@sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+				Name:       "index.docker.io/library/mariadb@sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+				Registry:   "index.docker.io",
+				Repository: "library/mariadb",
+				Tag:        "10.8.2",
+				Digest:     "sha256:8040983db146f729749081c6b216a19d52e0973134e2e34c0b4fd87f48bc15b0",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with valid custom image string",
+			args: args{
+				s: "docker.io/prom/mysqld-exporter@sha256:a8af600c3ef1c8df179b736b94d04dc5ec209be88407a4c1c1bd0fc6394f56e8",
+			},
+			want: &Image{
+				Original:   "docker.io/prom/mysqld-exporter@sha256:a8af600c3ef1c8df179b736b94d04dc5ec209be88407a4c1c1bd0fc6394f56e8",
+				Name:       "index.docker.io/prom/mysqld-exporter@sha256:a8af600c3ef1c8df179b736b94d04dc5ec209be88407a4c1c1bd0fc6394f56e8",
+				Registry:   "index.docker.io",
+				Repository: "prom/mysqld-exporter",
+				Tag:        "",
+				Digest:     "sha256:a8af600c3ef1c8df179b736b94d04dc5ec209be88407a4c1c1bd0fc6394f56e8",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with gcr image string",
+			args: args{
+				s: "gcr.io/google-containers/echoserver:1.10@sha256:cb5c1bddd1b5665e1867a7fa1b5fa843a47ee433bbb75d4293888b71def53229",
+			},
+			want: &Image{
+				Original:   "gcr.io/google-containers/echoserver:1.10@sha256:cb5c1bddd1b5665e1867a7fa1b5fa843a47ee433bbb75d4293888b71def53229",
+				Name:       "gcr.io/google-containers/echoserver@sha256:cb5c1bddd1b5665e1867a7fa1b5fa843a47ee433bbb75d4293888b71def53229",
+				Registry:   "gcr.io",
+				Repository: "google-containers/echoserver",
+				Tag:        "1.10",
+				Digest:     "sha256:cb5c1bddd1b5665e1867a7fa1b5fa843a47ee433bbb75d4293888b71def53229",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with valid image string without sha256 hash",
+			args: args{
+				s: "gcr.io/google-containers/echoserver:1.10",
+			},
+			want: &Image{
+				Original:   "gcr.io/google-containers/echoserver:1.10",
+				Name:       "gcr.io/google-containers/echoserver:1.10",
+				Registry:   "gcr.io",
+				Repository: "google-containers/echoserver",
+				Tag:        "1.10",
+				Digest:     "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with valid image string without sha256 hash",
+			args: args{
+				s: "kubedb/ui-server:1.10-v2",
+			},
+			want: &Image{
+				Original:   "kubedb/ui-server:1.10-v2",
+				Name:       "index.docker.io/kubedb/ui-server:1.10-v2",
+				Registry:   "index.docker.io",
+				Repository: "kubedb/ui-server",
+				Tag:        "1.10-v2",
+				Digest:     "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with valid image string with sha256 hash",
+			args: args{
+				s: "docker.io/kubedb/postgres-init@sha256:48aeb8859d8245d82788836918c817d1ff84dfc9e81612f3ee8e9e22b6e3e8a5",
+			},
+			want: &Image{
+				Original:   "docker.io/kubedb/postgres-init@sha256:48aeb8859d8245d82788836918c817d1ff84dfc9e81612f3ee8e9e22b6e3e8a5",
+				Name:       "index.docker.io/kubedb/postgres-init@sha256:48aeb8859d8245d82788836918c817d1ff84dfc9e81612f3ee8e9e22b6e3e8a5",
+				Registry:   "index.docker.io",
+				Repository: "kubedb/postgres-init",
+				Tag:        "",
+				Digest:     "sha256:48aeb8859d8245d82788836918c817d1ff84dfc9e81612f3ee8e9e22b6e3e8a5",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with official image without sha256 hash",
+			args: args{
+				s: "nginx:1.0.1",
+			},
+			want: &Image{
+				Original:   "nginx:1.0.1",
+				Name:       "index.docker.io/library/nginx:1.0.1",
+				Registry:   "index.docker.io",
+				Repository: "library/nginx",
+				Tag:        "1.0.1",
+				Digest:     "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with official image with sha256 hash",
+			args: args{
+				s: "nginx:1.0.1@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+			},
+			want: &Image{
+				Original:   "nginx:1.0.1@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+				Name:       "index.docker.io/library/nginx@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+				Registry:   "index.docker.io",
+				Repository: "library/nginx",
+				Tag:        "1.0.1",
+				Digest:     "sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Test with official image with latest tag and sha256 hash",
+			args: args{
+				s: "nginx:latest@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+			},
+			want: &Image{
+				Original:   "nginx:latest@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+				Name:       "index.docker.io/library/nginx@sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+				Registry:   "index.docker.io",
+				Repository: "library/nginx",
+				Tag:        "latest",
+				Digest:     "sha256:d586384381a0e6834cef73d432b1486f0b86334cb92e54256def62dd403f82ab",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseReference(tt.args.s, tt.args.opts...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseReference() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseReference() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
