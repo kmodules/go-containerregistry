babd96add8335ebf59fb8c535c4963fb35dfcf44
Tamal Saha <tamal@appscode.com>
2024-12-04 05:42:50 -0800

    Print unverified certificate
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- authn/digest.go
+++ authn/digest.go
@@ -19,6 +19,9 @@ package authn
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -38,6 +41,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 )
 
 var (
@@ -55,7 +59,7 @@ func AddInsecureRegistriesFlag(fs *pflag.FlagSet) {
 	if fs == nil {
 		fs = pflag.CommandLine
 	}
-	fs.StringSliceVar(&insecureRegistries, "insecure-registries", insecureRegistries, "List of registries to be used without TLS")
+	fs.StringSliceVar(&insecureRegistries, "insecure-registries", insecureRegistries, "List of registries to be used without TLS verification")
 }
 
 func ImageWithDigest(kc kubernetes.Interface, image string, k8sOpts *k8schain.Options) (string, error) {
@@ -75,10 +79,25 @@ func ImageWithDigest(kc kubernetes.Interface, image string, k8sOpts *k8schain.Op
 
 	digest, err := crane.Digest(image, crane.WithAuthFromKeychain(keyChain), WithTLSSkipVerify(image))
 	if err != nil {
+		var ce *tls.CertificateVerificationError
+		if errors.As(err, &ce) {
+			klog.ErrorS(err, "UnverifiedCertificates")
+			for _, cert := range ce.UnverifiedCertificates {
+				klog.Errorln(string(encodeCertPEM(cert)))
+			}
+		}
 		return "", err
 	}
 
 	return image + "@" + digest, nil
+}
+
+func encodeCertPEM(cert *x509.Certificate) []byte {
+	block := pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert.Raw,
+	}
+	return pem.EncodeToMemory(&block)
 }
 
 // CreateKeyChain a multi keychain based in input arguments
